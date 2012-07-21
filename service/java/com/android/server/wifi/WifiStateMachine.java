d3f227c6c5c1478c8508e5ed1d899f218b816674
Michaël Burtin <bukington@gmail.com>
2012-07-21 01:59:23 -0700

    wifi: support devices with SoftAp that don't require firmware reload
    * Set it with a prop these days instead
    
    Change-Id: I04a7ce21740c60a95d5293ee4567171836b09f52

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -3881,8 +3881,10 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
         }
 
         try {
-            mNwService.wifiFirmwareReload(mInterfaceName, "AP");
-            if (DBG) Log.d(TAG, "Firmware reloaded in AP mode");
+            if (!SystemProperties.getBoolean("ro.disableWifiApFirmwareReload", false)) {
+                mNwService.wifiFirmwareReload(mInterfaceName, "AP");
+                if (DBG) Log.d(TAG, "Firmware reloaded in AP mode");
+            }
         } catch (Exception e) {
             Log.e(TAG, "Failed to reload AP firmware " + e);
         }
