ae0b5cce21686ad00fd0a3d5aab35fe2152c4737
Vinit Deshapnde <vinitd@google.com>
2014-02-05 11:27:21 -0800

    Fix an inadvertent change in WifiNative
    wifinative.closeSupplicantConnection() closes sockets that were opened
    to talk to supplicant. It was accidentally removed when we moved to
    single socket connection to supplicant.
    
    Bug: 12344420
    Change-Id: I4242aff17baa91f3cdffaffb264c3bf4f93aaca3

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -2302,6 +2302,7 @@ public class WifiStateMachine extends StateMachine {
         * or when the driver is hung. Ensure supplicant is stopped here.
         */
         mWifiMonitor.killSupplicant(mP2pSupported);
+        mWifiNative.closeSupplicantConnection();
         sendSupplicantConnectionChangedBroadcast(false);
         setWifiState(WIFI_STATE_DISABLED);
     }
