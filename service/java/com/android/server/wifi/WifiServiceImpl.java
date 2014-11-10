9faca858b281db883b8343d5a79b42b095660a33
Nalla Kartheek <karthe@codeaurora.org>
2014-11-10 11:53:48 +0530

    Wi-Fi : Abort setWifiEnabled if WifiController not ready
    While executing setWifiEnabled, it send commands to WifiController.
    If WifiController is not started by this time, StateMachine reports
    crash which creates device reboot.
    Thus, check if WiFiController is started before invoking any
    command to the same. Indicate a failure in such an instance
    
    CRs-Fixed: 742857
    
    Change-Id: Ic40eb82522b67f7e606d4739c41da3b8ba2f3590

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -124,6 +124,8 @@ public final class WifiServiceImpl extends IWifiManager.Stub {
 
     final boolean mBatchedScanSupported;
 
+     private boolean mIsControllerStarted = false;
+
     /**
      * Asynchronous channel to WifiStateMachine
      */
@@ -351,6 +353,8 @@ public final class WifiServiceImpl extends IWifiManager.Stub {
         registerForBroadcasts();
 
         mWifiController.start();
+
+        mIsControllerStarted = true;
 
         // If we are already disabled (could be due to airplane mode), avoid changing persist
         // state here
@@ -664,6 +668,11 @@ public final class WifiServiceImpl extends IWifiManager.Stub {
             }
         } finally {
             Binder.restoreCallingIdentity(ident);
+        }
+
+        if (!mIsControllerStarted) {
+            Slog.e(TAG,"WifiController is not yet started, abort setWifiEnabled");
+            return false;
         }
 
         mWifiController.sendMessage(CMD_WIFI_TOGGLED);
