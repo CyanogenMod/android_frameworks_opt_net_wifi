e91eb1bc706fecb97407c00e21d32b1edb56dc88
Vinit Deshpande <vinitd@google.com>
2015-01-02 11:38:23 -0800

    Bring in passpoint code [DO NOT MERGE]
    This change brings in all the passpoint code from master
    to here - so that all development can be done with partners
    
    Change-Id: I9185c35c69630ffb9b4313f973893996051075ed

---
+++ service/java/com/android/server/wifi/passpoint/WifiPasspointService.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.wifi.passpoint;
+
+import android.content.Context;
+import android.util.Log;
+import com.android.server.SystemService;
+
+public final class WifiPasspointService extends SystemService {
+
+    private static final String TAG = "PasspointService";
+    final WifiPasspointServiceImpl mImpl;
+
+    public WifiPasspointService(Context context) {
+        super(context);
+        Log.i(TAG, "Creating " + Context.WIFI_PASSPOINT_SERVICE);
+        mImpl = new WifiPasspointServiceImpl(getContext());
+    }
+
+    @Override
+    public void onStart() {
+        Log.i(TAG, "Registering " + Context.WIFI_PASSPOINT_SERVICE);
+        publishBinderService(Context.WIFI_PASSPOINT_SERVICE, mImpl);
+    }
+
+    @Override
+    public void onBootPhase(int phase) {
+        if (phase == SystemService.PHASE_SYSTEM_SERVICES_READY) {
+            mImpl.systemServiceReady();
+        }
+    }
+}
