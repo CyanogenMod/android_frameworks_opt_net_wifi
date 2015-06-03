6eb4db053766557a4ebd2a969e0605b0eb6d68fc
Vinit Deshpande <vinitd@google.com>
2015-06-03 15:51:55 -0700

    Shut up a noisy log from WifiStateMachine [DO NOT MERGE]
    Bug: 20416721
    
    Change-Id: I675942232f2120a203c531d7ed8f214c3322d7dc

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -1635,7 +1635,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
                 mRxTime = stats.rx_time;
                 mRunningBeaconCount = stats.beacon_rx;
                 if (dbg) {
-                    loge(stats.toString());
+                    // loge(stats.toString());
                 }
             }
         }
