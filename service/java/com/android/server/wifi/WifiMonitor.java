6ae1a88c51b9db16ce82b4f4752cca54e961be51
Sunil Dutt <usdutt@codeaurora.org>
2015-06-05 19:25:48 +0530

    wifi : Fix for Wi-Fi grayed out issue
    During the initialization, WiFiMonitor shall try to open the
    control/monitor socket interface with the supplicant.On a
    failed attempt to open the control interface, WiFiMonitor
    thread shall trigger an event SUP_DISCONNECTION_EVENT to
    WiFiStateMachine with the expectation that the connection
    attempt restarts.In addition,it also removes the interface
    from the mIfaceMap.During the restart,WiFiMonitor does not
    proceed to open the control interface with the supplicant
    if the mIfaceMap does not point to the interface. Thus,do
    not remove the interface from mIfaceMap on a failed attempt
    to create a control interface with the supplicant to
    provision a reinitialization attempt.
    
    b/18798914
    Change-Id: Iefb8942a7561699557ea21cb87f4754da024139a

--- service/java/com/android/server/wifi/WifiMonitor.java
+++ service/java/com/android/server/wifi/WifiMonitor.java
@@ -609,7 +609,6 @@ public class WifiMonitor {
                         } catch (InterruptedException ignore) {
                         }
                     } else {
-                        mIfaceMap.remove(iface);
                         m.mStateMachine.sendMessage(SUP_DISCONNECTION_EVENT);
                         Log.e(TAG, "startMonitoring(" + iface + ") failed!");
                         break;
