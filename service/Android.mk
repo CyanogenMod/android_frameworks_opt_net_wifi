1a0319f2c6766a93090565d091ba13cf124f8590
Sravanthi Palakonda <srapal@codeaurora.org>
2015-07-27 16:38:09 +0530

    wifi: Support for AP with Chinese GBK Charset SSID
    It doesn't support Chinese GBK Charset in original Android.
    But there are some APs with Chinese GBK Charset SSID, it will result
    in that wifi can't display these APs nomally and connect to these APs.
    Here make a conversion in wifi JNI layer. When "SCAN" or "GET_NETWORK"
    command has the reply with Chinese GBK Charset SSID, convert GBK to
    UTF-8.
    
    Change-Id: Iaf01a0e44e8ad610408f1533723fc668c5a764b0

--- service/Android.mk
+++ service/Android.mk
@@ -86,6 +86,8 @@ LOCAL_C_INCLUDES += \
 	$(JNI_H_INCLUDE) \
 	$(call include-path-for, libhardware)/hardware \
 	$(call include-path-for, libhardware_legacy)/hardware_legacy \
+	external/icu/icu4c/source/common \
+	external/icu/icu4c/source/i18n \
 	libcore/include
 
 LOCAL_SHARED_LIBRARIES += \
@@ -96,13 +98,16 @@ LOCAL_SHARED_LIBRARIES += \
 	libhardware_legacy \
 	libandroid_runtime \
 	libnl \
-    libdl
+	libicuuc \
+	libicui18n \
+	libdl
 
 LOCAL_STATIC_LIBRARIES += libwifi-hal-stub
 LOCAL_STATIC_LIBRARIES += $(LIB_WIFI_HAL)
 
 LOCAL_SRC_FILES := \
 	jni/com_android_server_wifi_WifiNative.cpp \
+	jni/com_android_server_wifi_Gbk2Utf.cpp \
 	jni/jni_helper.cpp
 
 LOCAL_MODULE := libwifi-service
