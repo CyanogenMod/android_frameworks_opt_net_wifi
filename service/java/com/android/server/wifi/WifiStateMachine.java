75b9d88468fbfff6a486d6df41215909a2cc9a93
Sravanthi Palakonda <srapal@codeaurora.org>
2015-08-31 19:37:10 +0530

    wifi:Disable IpReachabilityMonitor feature through a config param
    IPreachabilityMonitor feature results in Wi-Fi disconnection. Thus ,
    disable this feature through a configuration parameter.
    
    Change-Id: I9a76a1a8f77762b524b323141c62aa8708ae70ea
    CRs-Fixed: 899312

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -210,6 +210,8 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
 
     private boolean mScreenOn = false;
 
+    private boolean mIsWiFiIpReachabilityEnabled ;
+
     /* Chipset supports background scan */
     private final boolean mBackgroundScanSupported;
 
@@ -1139,6 +1141,9 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
 
         mBackgroundScanSupported = mContext.getResources().getBoolean(
                 R.bool.config_wifi_background_scan_support);
+
+        mIsWiFiIpReachabilityEnabled = mContext.getResources().getBoolean(
+                R.bool.config_wifi_ipreachability_monitor);
 
         mPrimaryDeviceType = mContext.getResources().getString(
                 R.string.config_wifi_p2p_device_type);
@@ -8185,17 +8190,19 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
             // cause the roam to faile and the device to disconnect
             clearCurrentConfigBSSID("L2ConnectedState");
 
-            try {
-                mIpReachabilityMonitor = new IpReachabilityMonitor(
-                        mDataInterfaceName,
-                        new IpReachabilityMonitor.Callback() {
-                            @Override
-                            public void notifyLost(InetAddress ip, String logMsg) {
-                                sendMessage(CMD_IP_REACHABILITY_LOST, logMsg);
-                            }
-                        });
-            } catch (IllegalArgumentException e) {
-                Log.wtf("Failed to create IpReachabilityMonitor", e);
+            if (mIsWiFiIpReachabilityEnabled) {
+                try {
+                    mIpReachabilityMonitor = new IpReachabilityMonitor(
+                            mDataInterfaceName,
+                            new IpReachabilityMonitor.Callback() {
+                                @Override
+                                public void notifyLost(InetAddress ip, String logMsg) {
+                                    sendMessage(CMD_IP_REACHABILITY_LOST, logMsg);
+                                }
+                            });
+                } catch (IllegalArgumentException e) {
+                    Log.wtf("Failed to create IpReachabilityMonitor", e);
+                }
             }
         }
 
