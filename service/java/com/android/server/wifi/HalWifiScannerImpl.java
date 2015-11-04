f2f6f79242454ff9161f5af772292fa0180436f8
Mitchell Wills <mwills@google.com>
2015-11-03 17:16:09 -0800

    Make HAL oneshot wifi scans use wpa_supplicant
    Currently this code path isn't used, but will be enabled once the
    supplicant scanner implementation also supports the seperate oneshot
    scan api
    
    Change-Id: I23ce63172b82b9b4b824093885f78064fc064144

--- service/java/com/android/server/wifi/HalWifiScannerImpl.java
+++ service/java/com/android/server/wifi/HalWifiScannerImpl.java
@@ -16,15 +16,133 @@
 
 package com.android.server.wifi;
 
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiScanner;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 
 import com.android.server.wifi.WifiNative;
 
-public class HalWifiScannerImpl extends WifiScannerImpl {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * WifiScanner implementation that takes advantage of the gscan HAL API
+ * The gscan API is used to perform background scans and wpa_supplicant is used for onehot scans.
+ * @see com.android.server.wifi.WifiScannerImpl for more details on each method
+ */
+public class HalWifiScannerImpl extends WifiScannerImpl implements Handler.Callback {
+    private static final String TAG = "SupplicantWifiScannerImpl";
+    private static final boolean DBG = false;
+    private static final WifiScanner.ScanData[] EMPTY_SCAN_RESULT = new WifiScanner.ScanData[] {
+        new WifiScanner.ScanData(0, 0, new ScanResult[0])
+    };
+
     private final WifiNative mWifiNative;
-    public HalWifiScannerImpl(WifiNative wifiNative) {
+    private final Handler mEventHandler;
+    private boolean mReportSingleScanFullResults = false;
+    private WifiNative.ScanEventHandler mSingleScanEventHandler = null;
+    private WifiScanner.ScanData mLatestSingleScanResult = null;
+
+    public HalWifiScannerImpl(WifiNative wifiNative, Looper looper) {
         mWifiNative = wifiNative;
+        mEventHandler = new Handler(looper, this);
+
+        // We can't enable these until WifiStateMachine switches to using WifiScanner because
+        //   WifiMonitor only supports sending results to one listener
+        // TODO Enable these
+        // Also need to enable tests again when this is enabled
+        // WifiMonitor.getInstance().registerHandler(mWifiNative.getInterfaceName(),
+        //         WifiMonitor.SCAN_FAILED_EVENT, mEventHandler);
+        // WifiMonitor.getInstance().registerHandler(mWifiNative.getInterfaceName(),
+        //         WifiMonitor.SCAN_RESULTS_EVENT, mEventHandler);
     }
+
+    @Override
+    public boolean handleMessage(Message msg) {
+        switch(msg.what) {
+            case WifiMonitor.SCAN_FAILED_EVENT:
+                Log.w(TAG, "Single scan failed");
+                if (mSingleScanEventHandler != null) {
+                    // TODO indicate failure to caller
+                    mSingleScanEventHandler = null;
+                }
+                break;
+            case WifiMonitor.SCAN_RESULTS_EVENT:
+                pollLatestSingleScanData();
+                if (mSingleScanEventHandler != null) {
+                    if (mReportSingleScanFullResults && mLatestSingleScanResult != null) {
+                        for (ScanResult scanResult : mLatestSingleScanResult.getResults()) {
+                            mSingleScanEventHandler.onFullScanResult(scanResult);
+                        }
+                    }
+                    mSingleScanEventHandler.onScanResultsAvailable();
+                    mSingleScanEventHandler = null;
+                }
+                break;
+            default:
+                Log.e(TAG, "Received unknown message: type=" + msg.what);
+                // ignore unknown event
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean startSingleScan(WifiNative.ScanSettings settings,
+            WifiNative.ScanEventHandler eventHandler) {
+        if (mSingleScanEventHandler != null) {
+            Log.w(TAG, "A single scan is already running");
+            return false;
+        }
+        Set<Integer> freqs = new HashSet<>();
+        mReportSingleScanFullResults = false;
+        for (int i = 0; i < settings.num_buckets; ++i) {
+            WifiNative.BucketSettings bucketSettings = settings.buckets[i];
+            if ((bucketSettings.report_events
+                            & WifiScanner.REPORT_EVENT_FULL_SCAN_RESULT) != 0) {
+                mReportSingleScanFullResults = true;
+            }
+            if (bucketSettings.band == WifiScanner.WIFI_BAND_UNSPECIFIED) {
+                for (int j = 0; j < bucketSettings.num_channels; ++j) {
+                    WifiNative.ChannelSettings channel = bucketSettings.channels[j];
+                    freqs.add(channel.frequency);
+                }
+            } else {
+                WifiScanner.ChannelSpec[] channels =
+                    WifiChannelHelper.getChannelsForBand(bucketSettings.band);
+                for (WifiScanner.ChannelSpec channel : channels) {
+                    freqs.add(channel.frequency);
+                }
+            }
+        }
+
+        mSingleScanEventHandler = eventHandler;
+        if (!mWifiNative.scan(WifiNative.SCAN_WITHOUT_CONNECTION_SETUP, freqs)) {
+            mSingleScanEventHandler = null;
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public WifiScanner.ScanData getLatestSingleScanResults() {
+        return mLatestSingleScanResult;
+    }
+
+
+    private void pollLatestSingleScanData() {
+        List<ScanDetail> nativeResults = mWifiNative.getScanResults();
+        ScanResult[] results = new ScanResult[nativeResults.size()];
+        for (int i = 0; i < results.length; ++i) {
+            results[i] = nativeResults.get(i).getScanResult();
+        }
+        mLatestSingleScanResult = new WifiScanner.ScanData(0, 0, results);
+    }
+
     @Override
     public boolean getScanCapabilities(WifiNative.ScanCapabilities capabilities) {
         return mWifiNative.getScanCapabilities(capabilities);
--- service/java/com/android/server/wifi/WifiScannerImpl.java
+++ service/java/com/android/server/wifi/WifiScannerImpl.java
@@ -22,11 +22,18 @@ import android.os.Looper;
 
 import com.android.server.wifi.WifiNative;
 
+/**
+ * Defines the interface to the Wifi hardware required for the WifiScanner API
+ */
 public abstract class WifiScannerImpl {
+    /**
+     * Create the implementation that is most appropriate for the system.
+     * This method should only ever be called once.
+     */
     public static WifiScannerImpl create(Context context, Looper looper) {
         WifiNative wifiNative = WifiNative.getWlanNativeInterface();
         if (wifiNative.getScanCapabilities(new WifiNative.ScanCapabilities())) {
-            return new HalWifiScannerImpl(wifiNative);
+            return new HalWifiScannerImpl(wifiNative, looper);
         }
         else {
             return new SupplicantWifiScannerImpl(context, wifiNative, looper);
@@ -35,11 +42,42 @@ public abstract class WifiScannerImpl {
 
     public abstract boolean getScanCapabilities(WifiNative.ScanCapabilities capabilities);
 
+    /**
+     * Start a one time scan. This method should only be called when there is no scan going on
+     * (after a callback indicating that the previous scan succeeded/failed).
+     * @return if the scan paramaters are valid
+     * Note this may return true even if the parameters are not accepted by the chip because the
+     * scan may be scheduled async.
+     */
+    public abstract boolean startSingleScan(WifiNative.ScanSettings settings,
+            WifiNative.ScanEventHandler eventHandler);
+    /**
+     * Get the scan results of the most recent single scan. This should be called immediately when
+     * the scan success callback is receieved.
+     */
+    public abstract WifiScanner.ScanData getLatestSingleScanResults();
+
+    /**
+     * Start a background scan. Calling this method while a background scan is already in process
+     * will interrupt the previous scan settings and replace it with the new ones.
+     * @return if the scan paramaters are valid
+     * Note this may return true even if the parameters are not accepted by the chip because the
+     * scan may be scheduled async.
+     */
     public abstract boolean startBatchedScan(WifiNative.ScanSettings settings,
             WifiNative.ScanEventHandler eventHandler);
+    /**
+     * Stop the currently active background scan
+     */
     public abstract void stopBatchedScan();
+
     public abstract void pauseBatchedScan();
     public abstract void restartBatchedScan();
+
+    /**
+     * Get the latest cached scan results from the last scan event. This should be called
+     * immediately when the scan success callback is receieved.
+     */
     public abstract WifiScanner.ScanData[] getLatestBatchedScanResults(boolean flush);
 
     public abstract boolean setHotlist(WifiScanner.HotlistSettings settings,
