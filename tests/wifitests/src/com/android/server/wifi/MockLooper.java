b35c2105231b61606db78d4bf8a75ec31039db61
Etan Cohen <etancohen@google.com>
2016-01-11 10:24:00 -0800

    Merge the MockLooperAbstractTime functionality into baseline MockLooper
    The time functionality (post delayed event) of MockLooperAbstractTime is
    additive: i.e. base functionality (without posting delayed or moving time)
    is identical to MockLooper - hence no need to have a separate class.
    
    Change-Id: I01ae48712534a4c08003a5a6271a66c8c9bf984c

--- tests/wifitests/src/com/android/server/wifi/MockLooper.java
+++ tests/wifitests/src/com/android/server/wifi/MockLooper.java
@@ -21,6 +21,7 @@ import static org.junit.Assert.assertTrue;
 import android.os.Looper;
 import android.os.Message;
 import android.os.MessageQueue;
+import android.os.SystemClock;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
@@ -37,7 +38,10 @@ public class MockLooper {
 
     private static final Constructor<Looper> LOOPER_CONSTRUCTOR;
     private static final Field THREAD_LOCAL_LOOPER_FIELD;
-    private static final Method MESSAGE_QUEUE_NEXT_METHOD;
+    private static final Field MESSAGE_QUEUE_MESSAGES_FIELD;
+    private static final Field MESSAGE_NEXT_FIELD;
+    private static final Field MESSAGE_WHEN_FIELD;
+    private static final Method MESSAGE_MARK_IN_USE_METHOD;
 
     static {
         try {
@@ -45,8 +49,14 @@ public class MockLooper {
             LOOPER_CONSTRUCTOR.setAccessible(true);
             THREAD_LOCAL_LOOPER_FIELD = Looper.class.getDeclaredField("sThreadLocal");
             THREAD_LOCAL_LOOPER_FIELD.setAccessible(true);
-            MESSAGE_QUEUE_NEXT_METHOD = MessageQueue.class.getDeclaredMethod("next");
-            MESSAGE_QUEUE_NEXT_METHOD.setAccessible(true);
+            MESSAGE_QUEUE_MESSAGES_FIELD = MessageQueue.class.getDeclaredField("mMessages");
+            MESSAGE_QUEUE_MESSAGES_FIELD.setAccessible(true);
+            MESSAGE_NEXT_FIELD = Message.class.getDeclaredField("next");
+            MESSAGE_NEXT_FIELD.setAccessible(true);
+            MESSAGE_WHEN_FIELD = Message.class.getDeclaredField("when");
+            MESSAGE_WHEN_FIELD.setAccessible(true);
+            MESSAGE_MARK_IN_USE_METHOD = Message.class.getDeclaredMethod("markInUse");
+            MESSAGE_MARK_IN_USE_METHOD.setAccessible(true);
         } catch (NoSuchFieldException | NoSuchMethodException e) {
             throw new RuntimeException("Failed to initialize MockLooper", e);
         }
@@ -69,19 +79,74 @@ public class MockLooper {
         return mLooper;
     }
 
-    protected Message messageQueueNext() {
+    private Message getMessageLinkedList() {
         try {
-            return (Message) MESSAGE_QUEUE_NEXT_METHOD.invoke(mLooper.getQueue());
-        } catch (IllegalAccessException | InvocationTargetException e) {
-            throw new RuntimeException("Reflection error when getting next message", e);
+            MessageQueue queue = mLooper.getQueue();
+            return (Message) MESSAGE_QUEUE_MESSAGES_FIELD.get(queue);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Access failed in MockLooper: get - MessageQueue.mMessages",
+                    e);
         }
+    }
+
+    public void moveTimeForward(long milliSeconds) {
+        try {
+            Message msg = getMessageLinkedList();
+            while (msg != null) {
+                long updatedWhen = msg.getWhen() - milliSeconds;
+                if (updatedWhen < 0) {
+                    updatedWhen = 0;
+                }
+                MESSAGE_WHEN_FIELD.set(msg, updatedWhen);
+                msg = (Message) MESSAGE_NEXT_FIELD.get(msg);
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Access failed in MockLooper: set - Message.when", e);
+        }
+    }
+
+    private Message messageQueueNext() {
+        try {
+            long now = SystemClock.uptimeMillis();
+
+            Message prevMsg = null;
+            Message msg = getMessageLinkedList();
+            if (msg != null && msg.getTarget() == null) {
+                // Stalled by a barrier. Find the next asynchronous message in
+                // the queue.
+                do {
+                    prevMsg = msg;
+                    msg = (Message) MESSAGE_NEXT_FIELD.get(msg);
+                } while (msg != null && !msg.isAsynchronous());
+            }
+            if (msg != null) {
+                if (now >= msg.getWhen()) {
+                    // Got a message.
+                    if (prevMsg != null) {
+                        MESSAGE_NEXT_FIELD.set(prevMsg, MESSAGE_NEXT_FIELD.get(msg));
+                    } else {
+                        MESSAGE_QUEUE_MESSAGES_FIELD.set(mLooper.getQueue(),
+                                MESSAGE_NEXT_FIELD.get(msg));
+                    }
+                    MESSAGE_NEXT_FIELD.set(msg, null);
+                    MESSAGE_MARK_IN_USE_METHOD.invoke(msg);
+                    return msg;
+                }
+            }
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException("Access failed in MockLooper", e);
+        }
+
+        return null;
     }
 
     /**
      * @return true if there are pending messages in the message queue
      */
     public boolean isIdle() {
-        return !mLooper.getQueue().isIdle();
+        Message messageList = getMessageLinkedList();
+
+        return messageList != null && SystemClock.uptimeMillis() >= messageList.getWhen();
     }
 
     /**
