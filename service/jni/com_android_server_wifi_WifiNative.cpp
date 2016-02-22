202fdf96e1e0191be37a916d97116df35fe3cbfc
Wei Wang <weiwa@google.com>
2016-02-22 10:26:29 -0800

    Fix native crash caused by null rtt params.
    Bug:27227320
    
    Change-Id: I822045f2dd35aeb824cea0d0c0791480e0d1b80a

--- service/jni/com_android_server_wifi_WifiNative.cpp
+++ service/jni/com_android_server_wifi_WifiNative.cpp
@@ -1145,6 +1145,10 @@ static jboolean android_net_wifi_requestRange(
 
     wifi_interface_handle handle = getIfaceHandle(helper, cls, iface);
     ALOGD("sending rtt request [%d] = %p", id, handle);
+    if (params == NULL) {
+        ALOGE("ranging params are empty");
+        return false;
+    }
 
     wifi_rtt_config configs[MaxRttConfigs];
     memset(&configs, 0, sizeof(configs));
@@ -1209,6 +1213,11 @@ static jboolean android_net_wifi_cancelRange(
     JNIHelper helper(env);
     wifi_interface_handle handle = getIfaceHandle(helper, cls, iface);
     ALOGD("cancelling rtt request [%d] = %p", id, handle);
+
+    if (params == NULL) {
+        ALOGE("ranging params are empty");
+        return false;
+    }
 
     mac_addr addrs[MaxRttConfigs];
     memset(&addrs, 0, sizeof(addrs));
