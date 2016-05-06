CyanogenMod/android_frameworks_opt_net_wifi

commit bcbc449d47124ec8f73a38ffe228b97bd9b493bf

Unbreak framework roaming
Framework roams could transition to ObtainingIpState in which we
no longer send DHCPv4 RENEWs.  Owing to a bug we still started the
watchdog timer which would just fire 40 seconds later.

Instead, don't transition to ObtainingIpState except when actually
wanting to obtain IP configuration (gasp!).  We transition instead
back to ConnectedState and let IpReachabilityMonitor signal us if
we need to transition further.

Bug: 26991160
Change-Id: I535611aee542cf3dcebf57e618fcc73806797aac

service/java/com/android/server/wifi/WifiStateMachine.java
@@ -8071,21 +8071,24 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
             // cause the roam to fail and the device to disconnect.
             clearCurrentConfigBSSID("ObtainingIpAddress");
 
+            // Stop IpManager in case we're switching from DHCP to static
+            // configuration or vice versa.
+            //
+            // TODO: Only ever enter this state the first time we connect to a
+            // network, never on switching between static configuration and
+            // DHCP. When we transition from static configuration to DHCP in
+            // particular, we must tell ConnectivityService that we're
+            // disconnected, because DHCP might take a long time during which
+            // connectivity APIs such as getActiveNetworkInfo should not return
+            // CONNECTED.
+            stopIpManager();
+
             if (!mWifiConfigManager.isUsingStaticIp(mLastNetworkId)) {
-                // We used to do DHCPv4 RENEWs on framework roams.
-                // TODO: Investigate whether we should reinstitute this.
-                //
-                // When we get here after a roam, we will already have called
-                // mIpManager.confirmConfiguration() to ensure we're on the
-                // same network (in handling of SupplicantState.COMPLETED).
-                if (!isRoaming()) {
-                    mIpManager.stop();
-                    final IpManager.ProvisioningConfiguration prov =
-                            mIpManager.buildProvisioningConfiguration()
-                                .withPreDhcpAction()
-                                .build();
-                    mIpManager.startProvisioning(prov);
-                }
+                final IpManager.ProvisioningConfiguration prov =
+                        mIpManager.buildProvisioningConfiguration()
+                            .withPreDhcpAction()
+                            .build();
+                mIpManager.startProvisioning(prov);
                 obtainingIpWatchdogCount++;
                 logd("Start Dhcp Watchdog " + obtainingIpWatchdogCount);
                 // Get Link layer stats so as we get fresh tx packet counters
@@ -8093,17 +8096,6 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
                 sendMessageDelayed(obtainMessage(CMD_OBTAINING_IP_ADDRESS_WATCHDOG_TIMER,
                         obtainingIpWatchdogCount, 0), OBTAINING_IP_ADDRESS_GUARD_TIMER_MSEC);
             } else {
-                // Stop IpManager in case we're switching from static
-                // configuration to DHCP.
-                //
-                // TODO: Only ever enter this state the first time we
-                // connect to a network, never on changing from static
-                // configuration to DHCP. When we transition from static
-                // configuration to DHCP, we must tell ConnectivityService
-                // that we're disconnected, because DHCP might take a long
-                // time, during which connectivity APIs such as
-                // getActiveNetworkInfo should not return CONNECTED.
-                stopIpManager();
                 StaticIpConfiguration config = mWifiConfigManager.getStaticIpConfiguration(
                         mLastNetworkId);
                 if (config.ipAddress == null) {
@@ -8279,7 +8271,16 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
                         mWifiQualifiedNetworkSelector.enableBssidForQualityNetworkSelection(
                                 mLastBssid, true);
                         sendNetworkStateChangeBroadcast(mLastBssid);
-                        transitionTo(mObtainingIpState);
+
+                        // We used to transition to ObtainingIpState in an
+                        // attempt to do DHCPv4 RENEWs on framework roams.
+                        // DHCP can take too long to time out, and we now rely
+                        // upon IpManager's use of IpReachabilityMonitor to
+                        // confirm our current network configuration.
+                        //
+                        // mIpManager.confirmConfiguration() is called within
+                        // the handling of SupplicantState.COMPLETED.
+                        transitionTo(mConnectedState);
                     } else {
                         messageHandlingStatus = MESSAGE_HANDLING_STATUS_DISCARD;
                     }

commit a4e4bd02b4d77de2ad69fcdf79a32c6fbb395513

Clear config BSSID after successful roaming
Clear the config BSSID after roaming completed and before entering
ConnectedState. Wifi chipset may roam from this point on and
having the BSSID specified by QNS would cause the roam to fail
and the device to disconnect.

We used to rely on the fact that entering ObtainingIpState
clears the config BSSID. But this assumption was broken by
commit bcbc449d47124ec8f73a, which changed from transition
to ObtainingIpState to ConnectedState.

Bug: 28520770
Test: Wifi unit tests, manual tests
Change-Id: I09a2d5d1132ff597c63a324a24d13719be384603

service/java/com/android/server/wifi/WifiStateMachine.java
@@ -7091,6 +7091,15 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiRss
                         reportConnectionAttemptEnd(
                                 WifiMetrics.ConnectionEvent.FAILURE_NONE,
                                 WifiMetricsProto.ConnectionEvent.HLF_NONE);
+
+                        // We must clear the config BSSID, as the wifi chipset may decide to roam
+                        // from this point on and having the BSSID specified by QNS would cause
+                        // the roam to fail and the device to disconnect.
+                        // When transition from RoamingState to DisconnectingState or
+                        // DisconnectedState, the config BSSID is cleared by
+                        // handleNetworkDisconnect().
+                        clearCurrentConfigBSSID("RoamingCompleted");
+
                         // We used to transition to ObtainingIpState in an
                         // attempt to do DHCPv4 RENEWs on framework roams.
                         // DHCP can take too long to time out, and we now rely