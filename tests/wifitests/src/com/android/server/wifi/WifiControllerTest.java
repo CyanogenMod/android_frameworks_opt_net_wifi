8ec7811cb6db1d605ed95060f3f792f2221cee88
Rebecca Silberstein <silberst@google.com>
2016-05-06 17:01:04 -0700

    WifiControllerTest: cleanup style issues
    Clean up some style issues for WifiControllerTest.
    
    This is in preparation for additional tests being added for the bug fix.
    
    BUG: 28434530
    Change-Id: Idfcc51e2d30753051b9d8e2ec066035f771f0339

--- tests/wifitests/src/com/android/server/wifi/WifiControllerTest.java
+++ tests/wifitests/src/com/android/server/wifi/WifiControllerTest.java
@@ -1,6 +1,28 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.android.server.wifi;
 
+import static com.android.server.wifi.WifiController.CMD_EMERGENCY_CALL_STATE_CHANGED;
+import static com.android.server.wifi.WifiController.CMD_EMERGENCY_MODE_CHANGED;
+import static com.android.server.wifi.WifiController.CMD_WIFI_TOGGLED;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -20,13 +42,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
 
-import static com.android.server.wifi.WifiController.CMD_EMERGENCY_CALL_STATE_CHANGED;
-import static com.android.server.wifi.WifiController.CMD_EMERGENCY_MODE_CHANGED;
-import static com.android.server.wifi.WifiController.CMD_WIFI_TOGGLED;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
+/**
+ * Test WifiController for changes in and out of ECM and SoftAP modes.
+ */
 @SmallTest
 public class WifiControllerTest {
 
@@ -217,60 +235,4 @@ public class WifiControllerTest {
         mLooper.dispatchAll();
         assertEquals("DeviceActiveState", getCurrentState().getName());
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
